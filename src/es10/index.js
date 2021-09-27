80095b95503ae01a68f892ff81deb1f09bed76aa
Zaier Vera <veraalber989@gmail.com>
2021-09-27 12:33:24 -0300

    Estas fueron las funciones que se agregaron en es10

--- src/es10/index.js
+++ src/es10/index.js
@@ -1,3 +1,32 @@
 let array = [1,2,3, [1,2,3, [1,2,3]]];
 
-console.log(array.flatMap((value) => (value, value *2)))
+console.log(array.flat())
+
+
+let array = [1,2,3,4,5];
+
+console.log(array.flatMap(value => [value, value * 2]));
+
+
+//Eliminar espacios vacios
+let hello = '               hello World';
+console.log(hello);
+console.log(hello.trimStart());
+
+
+//Se agrego también la opción de no usar el argumento error cuando utiliamos try/catch
+try {
+    
+} catch {
+    error;
+}
+
+
+// Se pueden transformar de Arreglos a Objetos y viceversa
+let entries = [['name', 'Zaier'], ['age', 32]];
+
+console.log(Object.fromEntries(entries))
+
+let mySymbol = `My Symbol`;
+let symbol = Symbol(mySymbol);
+console.log(symbol.description)
