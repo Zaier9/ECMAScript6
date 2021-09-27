37cd1bc823e1b8a60af2db02879f3f2e837e6594
Zaier Vera <veraalber989@gmail.com>
2021-09-27 15:39:36 -0300

    Agregando las nuevas funcionalidades de es12

---
+++ src/es12/index.js
@@ -0,0 +1,57 @@
+// replace ya existia
+
+const string = "JavaScript es maravilloso, con JavaScript estamos creando el futuro de la web";
+const replacedString = string.replace("JavaScript", "Python");
+console.log(replacedString);
+
+
+// replaceAll es lo nuevo
+
+const replacedString2 = string.replaceAll("JavaScript", "Python");
+console.log(replacedString2);
+
+//Output
+//Python es maravilloso, con JavaScript estamos creando el futuro de la web
+//Python es maravilloso, con Python estamos creando el futuro de la web
+
+
+// Metodos privados
+class Message {
+    #show(val) { //con # convertimos al metodo en privado
+        console.log(val);
+    };
+    get #add(val) {
+        ...
+    }
+};
+
+const message = new Message();
+message.show('Hola!');
+
+
+// Promise Any
+const promise1 = new Promise((resolve, reject) => reject('1'));
+const promise2 = new Promise((resolve, reject) => resolve('2'));
+const promise3 = new Promise((resolve, reject) => resolve('3'));
+
+Promise.any([promise1, promise2, promise3])
+    .then(response => console.log(response));
+
+
+//WeakRef(element);
+class AnyClass {
+    constructor(element) {
+        this.ref = new WeakRef
+    }
+    {...}
+}
+
+
+//Nuevos operadores logicos
+let isTrue = true;
+let isFalse = false;
+console.log(isTrue &&= isFalse);
+
+let isTrue = true;
+let isFalse = false;
+console.log(isTrue ||= isFalse);
